24b4c1f2c844576b8d1b02a475d3d5c5bc2d81a5
Ephrim <mirhpe17@gmail.com>
2024-12-09 15:30:37 +0530

    Adding modal view with two textfields and date picker. Next step on submitting we are planning to create new expense and update the list.

--- lib/expense_app/new_expense.dart
+++ lib/expense_app/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -9,29 +10,78 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
 
-var expenseName;
+var expenseDate = 'date';
+var expenseName = '';
+var expenseAmount = '';
 
-void updateExpenseName(String value) {
-  expenseName = value;
+  void expenseNameFieldChanged(value){
+    expenseName = value;
+  }
+
+  void expenseAmountFieldChanged(value){
+    expenseAmount = value;
+  }
+
+  void addExpenseAction() {
+  
+  }
+
+Future<void> presentDatePicker() async {
+  final now = DateTime.now();
+  final firstDate = DateTime(now.year -1, now.month, now.day);
+  final lastDate = now;
+  final pickedDate = await showDatePicker(context: context, firstDate: firstDate, lastDate: lastDate);
+  final dateFormatter = DateFormat.yMd();
+  final formattedDate = dateFormatter.format(pickedDate!);
+  setState(() {
+    expenseDate = formattedDate;
+  });
 }
-
-
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
       child: Column(
         children: [
           TextField(
-            onChanged: updateExpenseName,
+            onChanged: expenseNameFieldChanged,
             maxLength: 50,
             keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-              label: Text('expense name')
+            decoration: const InputDecoration(
+              label: Text('Expense name')
             ),
           ),
-          ElevatedButton(onPressed: () {}, child: Text('Add Expense'))
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  onChanged: expenseAmountFieldChanged,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: '\$',
+                    label: Text('Amount')
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16.0),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(expenseDate),
+                    IconButton(onPressed: presentDatePicker, icon: const Icon(Icons.calendar_month))
+                  ],
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: ElevatedButton(onPressed: addExpenseAction, child: const Text('Add Expense')),
+          ),
+          const SizedBox(height: 10,),
+          ElevatedButton(onPressed: (){}, child: const Text('Cancel'))
         ],
       ),
     );
