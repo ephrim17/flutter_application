1b19b03c307d24c389bd703f23f106de8858d8b3
Ephrim <mirhpe17@gmail.com>
2025-01-04 15:33:25 +0530

    Locking the device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application/app_starter_menu.dart';
 
 void main() {
-  runApp(const AppStarterMenu());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((fun){
+    runApp(const AppStarterMenu());
+  });
 }
