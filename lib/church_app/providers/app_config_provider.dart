c000c08518fe659e29257803051248c81b37be96
Ephrim Daniel <mirhpe17@gmail.com>
2026-02-08 21:44:56 +0530

    [Church App] - config steaming issue fixed

--- lib/church_app/providers/app_config_provider.dart
+++ lib/church_app/providers/app_config_provider.dart
@@ -2,11 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_application/church_app/models/app_config_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final appConfigProvider = FutureProvider<AppConfig>((ref) async {
-  final snapshot = await FirebaseFirestore.instance
+final appConfigProvider = StreamProvider<AppConfig>((ref) {
+  return FirebaseFirestore.instance
       .collection('config')
       .doc('app')
-      .get();
-
-  return AppConfig.fromMap(snapshot.data()!);
+      .snapshots()
+      .map((snapshot) {
+        return AppConfig.fromMap(snapshot.data()!);
+      });
 });
+
